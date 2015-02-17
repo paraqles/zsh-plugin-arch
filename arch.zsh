b4145007ed80fcac6c72d5ec6607a02e041fcda7
paraqles <paraqles@dthal.net>
2015-02-17 16:50:52 +0100

    Update of arch

--- arch.zsh
+++ arch.zsh
@@ -1,5 +1,2 @@
-ZSH_PLUGIN_ARCH_DIR=$ZSH_PLUGINS_DIR/arch
+ZSH_EXTENSION_ARCH_DIR=$ZSH_EXTENSIONS_DIR/arch
 
-source $ZSH_PLUGIN_ARCH_DIR/aliases.zsh
-source $ZSH_PLUGIN_ARCH_DIR/functions.zsh
-source $ZSH_PLUGIN_ARCH_DIR/key_binding.zsh
--- functions.zsh
+++
@@ -1,3 +0,0 @@
-for fc in `ls $ZSH_PLUGIN_ARCH_DIR/func.*.zsh`; do
-  source $fc
-done
